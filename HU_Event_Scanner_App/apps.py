18bae5cf78f1d74a554a583328c4879591a68ac4
Sprunger <sprungert@huntington.edu>
2019-10-06 15:16:02 -0400

    Add HU_Event_Scanner_App Folder

---
+++ HU_Event_Scanner_App/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class HuEventScannerAppConfig(AppConfig):
+    name = 'HU_Event_Scanner_App'
