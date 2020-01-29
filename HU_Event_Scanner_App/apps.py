3c87188764ed944347bc2a39993217fcc297bc55
LandonBaer721 <55096622+LandonBaer721@users.noreply.github.com>
2020-01-29 12:04:01 -0500

    Delete apps.py

--- HU_Event_Scanner_App/apps.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class HuEventScannerAppConfig(AppConfig):
-    name = 'HU_Event_Scanner_App'
